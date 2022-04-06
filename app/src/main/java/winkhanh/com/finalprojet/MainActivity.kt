948655a9431ad41ff8416768c1590f90a86486ce
winkhanh <giakhanhnk2000@gmail.com>
2022-04-06 18:18:11 -0400

    set up google map

--- app/src/main/java/winkhanh/com/finalprojet/MainActivity.kt
+++ app/src/main/java/winkhanh/com/finalprojet/MainActivity.kt
@@ -4,8 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import winkhanh.com.finalprojet.fragments.MapsFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,5 +33,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val flContainer : FrameLayout = findViewById(R.id.flContainer)
+        val fragment : Fragment = MapsFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit()
     }
 }
