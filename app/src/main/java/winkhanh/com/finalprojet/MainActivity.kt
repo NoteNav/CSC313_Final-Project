037095be7d6888e3d9635039b517e227e64d9ff0
winkhanh <giakhanhnk2000@gmail.com>
2022-04-11 21:08:51 -0400

    add navbar

--- app/src/main/java/winkhanh/com/finalprojet/MainActivity.kt
+++ app/src/main/java/winkhanh/com/finalprojet/MainActivity.kt
@@ -2,16 +2,25 @@ package winkhanh.com.finalprojet
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.gms.maps.MapFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import winkhanh.com.finalprojet.fragments.MapsFragment
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottomNav : BottomNavigationView
+    val fragmentManager: FragmentManager = supportFragmentManager
 
+    val fragmentM : Fragment = MapsFragment()
+    val fragmentN : Fragment = MapsFragment() //replace this for the Note Fragment
+    val fragmentP : Fragment = MapsFragment() //replace this for the Profile Fragment
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -33,8 +42,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val flContainer : FrameLayout = findViewById(R.id.flContainer)
         val fragment : Fragment = MapsFragment()
         supportFragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit()
+        bottomNav = findViewById(R.id.bottomNav)
+        bottomNav.setOnItemSelectedListener { item ->
+            lateinit var fragment: Fragment
+            when (item.itemId) {
+                R.id.action_map -> {
+                    fragment = fragmentM
+                    Log.d("Main","Pick Map")
+                }
+                R.id.action_note -> {
+                    fragment = fragmentN
+                    Log.d("Main","Pick Note")
+                }
+                R.id.action_profile -> {
+                    fragment = fragmentP
+                    Log.d("Main","Pick Profile")
+                }
+                else -> fragment = fragmentM
+            }
+            fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit()
+            true
+        }
+        bottomNav.selectedItemId = R.id.action_map
     }
 }
