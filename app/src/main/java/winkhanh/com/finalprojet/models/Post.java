9bfcf93ab2024c49b71f80ace4646d44a73ac47d
winkhanh <giakhanhnk2000@gmail.com>
2022-04-22 18:43:50 -0400

    finish map

--- app/src/main/java/winkhanh/com/finalprojet/models/Post.java
+++ app/src/main/java/winkhanh/com/finalprojet/models/Post.java
@@ -1,4 +1,6 @@
-package winkhanh.com.finalprojet;
+package winkhanh.com.finalprojet.models;
+
+import android.util.Pair;
 
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
@@ -11,20 +13,22 @@ public class Post extends ParseObject {
     public static final String KEY_TITLE = "Title";
     public static final String KEY_USER = "User";
     public static final String KEY_CREATED_KEY = "createdAt";
+    public static final String KEY_LAT = "latitude";
+    public static final String KEY_LONG = "longitude";
 
-    public String getKeyDetail(){
+    public String getDetail(){
         return getString(KEY_DETAIL);
     }
 
-    public void setKeyDetail(String Detail) {
+    public void setDetail(String Detail) {
         put(KEY_DETAIL, Detail);
     }
 
-    public String getKeyTitle(){
+    public String getTitle(){
         return getString(KEY_TITLE);
     }
 
-    public void setKeyTitle(String Title) {
+    public void seTitle(String Title) {
         put(KEY_DETAIL, Title);
     }
 
@@ -35,5 +39,9 @@ public class Post extends ParseObject {
     public void setUser(ParseUser user){
         put(KEY_USER, user);
     }
+
+    public Pair<Double, Double> getLocation() { return new Pair<Double, Double>(getNumber(KEY_LAT).doubleValue(),getNumber(KEY_LONG).doubleValue());}
+
+    public void setLocation(Number latitude, Number longitude){ put(KEY_LAT, latitude); put(KEY_LONG, longitude);}
 
 }
