7b9d3959e782ebbfecb19ee15d2cb3e0bcdcd0ec
Aouncute <amornratajmo@gmail.com>
2022-04-29 19:17:03 -0400

    Profile posts

--- app/src/main/java/winkhanh/com/finalprojet/ParseApplication.java
+++ app/src/main/java/winkhanh/com/finalprojet/ParseApplication.java
@@ -14,6 +14,11 @@ public class ParseApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
+
+
+
 
         ParseObject.registerSubclass(Post.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
---
+++ app/src/main/java/winkhanh/com/finalprojet/PostsAdapter.java
@@ -0,0 +1,86 @@
+package winkhanh.com.finalprojet;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.parse.ParseFile;
+
+import java.util.List;
+
+import winkhanh.com.finalprojet.models.Post;
+
+public class PostsAdapter  extends RecyclerView.Adapter<PostsAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Post> posts;
+
+    public PostsAdapter(Context context, List<Post> posts) {
+        this.context = context;
+        this.posts = posts;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Post post = posts.get(position);
+        holder.bind(post);
+    }
+
+    @Override
+    public int getItemCount() {
+        return posts.size();
+    }
+
+    // Clean all elements of the recycler
+    public void clear(){
+        posts.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items -- change to type used
+    public void addAll(List<Post> postList) {
+        posts.addAll(postList);
+        notifyDataSetChanged();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView tvUsername;
+        private TextView tvDetail;
+        private TextView tvTitle;
+        private TextView tvCreatedAt;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvUsername = itemView.findViewById(R.id.tvUsername);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvDetail = itemView.findViewById(R.id.tvDetail);
+            tvCreatedAt = itemView.findViewById(R.id.tvCreatedAt);
+        }
+
+        public void bind(Post post) {
+            // Bind the post data to the view elements
+            tvTitle.setText(post.getTitle());
+            tvDetail.setText(post.getDetail());
+            tvUsername.setText(post.getUser().getUsername());
+            tvCreatedAt.setText(post.getCreatedAt().toString());
+
+            }
+
+    }
+}
+
